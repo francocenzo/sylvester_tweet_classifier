465053ddd2026b56e2a4ad6db426b6d65a975c20
francocenzo <107302086+francocenzo@users.noreply.github.com>
2022-09-14 00:54:16 +0200

    Added svm as additional classifier to test

--- sylvester/training.py
+++ sylvester/training.py
@@ -5,7 +5,7 @@ from sklearn import naive_bayes as nb, model_selection as ms
 from sklearn.feature_extraction.text import CountVectorizer
 
 # from sklearn.ensemble import RandomForestClassifier
-
+from sklearn import svm
 from sylvester.analyzer import calculate_metrics
 from sylvester.data import get_data, generate_stopwords, save_training
 
@@ -13,18 +13,19 @@ from sylvester.data import get_data, generate_stopwords, save_training
 def get_classifier(train_data, train_labels, save=False, load=False):
     # change here if we want to use another naive bayes algorithm:
     # https: // scikit - learn.org / stable / modules / naive_bayes.html
-    classifier = nb.BernoulliNB()
+    #classifier = nb.BernoulliNB()
     # classifier = RandomForestClassifier()
+    classifier = svm.SVC(kernel='linear', random_state=1, C=0.1)
     classifier.fit(train_data, train_labels)
     return classifier
 
 
 def transform_to_matrix(data_test, data_train, stopwords, save_feature=False):
     # transform training data to matrices
-    vectorizer = CountVectorizer(tokenizer=str.split, stop_words=stopwords, ngram_range=[1, 3])
+    vectorizer = CountVectorizer(tokenizer=str.split, stop_words=stopwords, ngram_range=[1, 1])
     train_matrix = vectorizer.fit_transform(data_train)
 
-    # Ausgabe der bzgl der letzten Frage:
+    # Ausgabe bzgl der letzten Frage:
     feature_names = list(vectorizer.get_feature_names_out())
     print("\n\nVectorizer")
     print(feature_names)
